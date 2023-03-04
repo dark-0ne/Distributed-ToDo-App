7ea3f4a4af5700720d1be066603620893ec49c87
dark-0ne <17182126+dark-0ne@users.noreply.github.com>
2023-03-04 15:26:27 +0200

    Set up Redis cluster

--- ansible/run-all-playbooks.sh
+++ ansible/run-all-playbooks.sh
@@ -1,4 +1,5 @@
-ansible-galaxy install -r requirements.ssh
+#!/bin/bash
+ansible-galaxy install -r requirements.yml
 
 ansible-playbook -i inventory/hosts.ini mongodb.yml
 ansible-playbook -i inventory/hosts.ini redis.yml
