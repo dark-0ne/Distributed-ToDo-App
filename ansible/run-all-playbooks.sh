fc406cd7271993b9e1ec3cc6b473cf20f6b5cf51
Mehrdad Kaheh <kahe.mehrdad@gmail.com>
2023-03-10 15:16:34 +0200

    strenthening the fortress

--- ansible/run-all-playbooks.sh
+++ ansible/run-all-playbooks.sh
@@ -6,7 +6,7 @@ ansible-playbook -i inventory/hosts.ini mongo_express.yml && \
 ansible-playbook -i inventory/hosts.ini redis.yml && \
 ansible-playbook -i inventory/hosts.ini nginx.yml && \
 ansible-playbook -i inventory/hosts.ini flask.yml && \
-ansible-playbook -i inventory/hosts.ini portainer.yml && \
+ansible-playbook -i inventory/hosts.ini portainer_server.yml && \
 xdg-open https://portainer.csilabs.eu/ > /dev/null 2>&1
 
 
